af5cd09b23bdd527ec3ebe530e4f5e987ea5d529
ketan-cloverinfotech <ketan.thombare@cloverinfotech.com>
2026-03-30 18:09:25 +0530

    Update base path in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Replace 'coe-mom-generator' with your actual GitHub repo name
 export default defineConfig({
   plugins: [react()],
-  base: '/coe-mom-generator/',
+  base: '/COE-weekly-status/',
 })
